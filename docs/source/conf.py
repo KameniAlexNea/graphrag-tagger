201adb4721477097eee28db26068bf4574d8d627
eak <eak@ivalua.com>
2025-02-27 12:42:22 +0000

    Refactor documentation structure by moving source files to a dedicated directory and updating build configurations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))  # Adjust the path if needed
+sys.path.insert(0, os.path.abspath('../..'))  # Adjust the path if needed
 
 
 # Configuration file for the Sphinx documentation builder.
